614325f232a69821a17bb3ff7df0544b8b5693f2
Mishaproh <mishaprohorenko96@gmail.com>
2021-05-10 21:20:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,33 @@
 
 //#1
 
-
-
 function calcSumm(a, b, c) {
-}
-console.log(calcSumm(3, 4, 2));
+    if (arguments.length === 0) {
+        return 'значения не введены';
+    }
+    let result = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        result += arguments[i];
+    }
+    return result;
+};
+console.log(calcSumm(2, 1, 3));
+console.log(calcSumm());
+console.log(calcSumm('5', '2', '8'));
+
+//========================================================================
+
+//#2
+
+// function sortStrings (firstString,secondString,thirdString){
+//     let strings = [firstString , secondString, thirdString];    
+//     strings.sort();
+//     console.log(`${strings[0]} ${strings[1]} ${strings[2]}`);
+
+// }
+// sortStrings ('Роман'.toLowerCase(), 'федор'.toLowerCase(), 'Екатерина'.toLowerCase());
+
+
 
 //========================================================================
 
@@ -30,3 +52,16 @@ console.log(calcSumm(3, 4, 2));
 
 //========================================================================
 
+//#4
+
+
+// function calcTwoNumber(a, b) {
+//     if (b === '' || b === null || b === undefined) {
+//         return Math.pow(a, 2)
+//     }
+//     return Math.pow(a, b)
+// }
+// console.log(calcTwoNumber(3, 2))
+
+
+//========================================================================
